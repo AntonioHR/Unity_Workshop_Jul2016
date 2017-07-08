2ac19f62cfaeac0eb0e5f9f3b8c44591d9a73b45
Antonio Harger Rivero <antonio.harger@gmail.com>
2017-07-07 21:13:08 -0300

    Added two first Examples

---
+++ Assets/2-Menu/Scripts/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public Vector3 axis;
+    [Space()]
+
+    public bool randomSpeed;
+    public AnimationCurve speedDistribution;
+    [Range(-360, 360)]
+    public float baseSpeed;
+    [Space()]
+    public bool randomStart;
+	void Start () {
+        if (randomSpeed)
+            baseSpeed = speedDistribution.Evaluate(Random.value) * baseSpeed;
+        if (randomStart)
+            transform.Rotate(axis, Random.Range(0, 360));
+	}
+	
+	void Update () {
+        transform.Rotate(axis, baseSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/MainMenuSceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSceneChanger : MonoBehaviour {
+    public int gameSceneIndex = 0;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void GoToGameScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(gameSceneIndex);
+    }
+}
